6ea922833e1d9bfec0267534c9677e1853de23be
Dennis Periquet <dperique@redhat.com>
2022-05-11 08:43:38 -0400

    use interface for abstration

--- test/extended/operators/check_crd_status.go
+++ test/extended/operators/check_crd_status.go
@@ -134,6 +134,110 @@ func checkStatusInSchema(crdItemList []apiextensionsv1.CustomResourceDefinition)
 	return failures
 }
 
+// checkStatusInSchema returns a list of names of CRDs that don't have a "status" in the CRD schema.
+// For now, it ignores the ones that are currently failing.
+func checkFunc(checkObj checkerInt, crdItemList []apiextensionsv1.CustomResourceDefinition) []string {
+
+	// These CRDs, at the time this test was written, do not have a "status" in the CRD schema
+	// and subresource.status.
+	// These can be skipped for now but we don't want the number to increase.
+	// These CRDs should be tidied up over time.
+	//
+	exceptionsList := checkObj.getExceptions()
+
+	failures := []string{}
+	for _, crdItem := range crdItemList {
+
+		// This test is interested only in CRDs that end with "openshift.io".
+		if !strings.HasSuffix(crdItem.ObjectMeta.Name, "openshift.io") {
+			continue
+		}
+
+		crdName := crdItem.ObjectMeta.Name
+
+		// Skip CRDs in the exceptions list for now.
+		if exceptionsList.Has(crdName) {
+			continue
+		}
+
+		// Iterate through all versions of the CustomResourceDefinition Spec looking for one with
+		// a schema status element,
+		foundStatusInSchema := false
+		var i int
+		for i = 0; i < len(crdItem.Spec.Versions); i++ {
+			if _, ok := crdItem.Spec.Versions[i].Schema.OpenAPIV3Schema.Properties["status"]; ok {
+				foundStatusInSchema = true
+				break
+			}
+		}
+
+		failed, failureText := checkObj.processFoundState(foundStatusInSchema, crdItem, i)
+		if failed {
+			failures = append(failures, failureText)
+		}
+	}
+
+	return failures
+}
+
+type checkSubresourceStatusType struct{}
+type checkStatusInSchemaType struct{}
+
+func (c checkSubresourceStatusType) processFoundState(foundStatusInSchema bool, crdItem apiextensionsv1.CustomResourceDefinition, versionIndex int) (bool, string) {
+	if !foundStatusInSchema {
+		return true, fmt.Sprintf("CRD %s has no 'status' element in its schema", crdItem.ObjectMeta.Name)
+	}
+	return false, ""
+}
+
+func (c checkSubresourceStatusType) getExceptions() sets.String {
+	return sets.NewString(
+		"networks.config.openshift.io",
+		"networks.operator.openshift.io",
+		"operatorpkis.network.operator.openshift.io",
+		"profiles.tuned.openshift.io",
+		"tuneds.tuned.openshift.io")
+}
+
+func (c checkStatusInSchemaType) processFoundState(foundStatusInSchema bool, crdItem apiextensionsv1.CustomResourceDefinition, versionIndex int) (bool, string) {
+	if foundStatusInSchema {
+		if !(crdItem.Spec.Versions[versionIndex].Subresources != nil && crdItem.Spec.Versions[versionIndex].Subresources.Status != nil) {
+			return true, fmt.Sprintf("CRD %s has a status in the schema but no subresource.status", crdItem.ObjectMeta.Name)
+		}
+	}
+	return false, ""
+}
+
+func (c checkStatusInSchemaType) getExceptions() sets.String {
+	return sets.NewString(
+		"builds.config.openshift.io",
+		"clusternetworks.network.openshift.io",
+		"consoleclidownloads.console.openshift.io",
+		"consoleexternalloglinks.console.openshift.io",
+		"consolelinks.console.openshift.io",
+		"consolenotifications.console.openshift.io",
+		"consoleplugins.console.openshift.io",
+		"consolequickstarts.console.openshift.io",
+		"consoleyamlsamples.console.openshift.io",
+		"egressnetworkpolicies.network.openshift.io",
+		"hostsubnets.network.openshift.io",
+		"imagecontentpolicies.config.openshift.io",
+		"imagecontentsourcepolicies.operator.openshift.io",
+		"machineconfigs.machineconfiguration.openshift.io",
+		"netnamespaces.network.openshift.io",
+		"rangeallocations.security.internal.openshift.io",
+		"rolebindingrestrictions.authorization.openshift.io",
+		"securitycontextconstraints.security.openshift.io")
+}
+
+type checkerType struct {
+	checkerInt
+}
+type checkerInt interface {
+	getExceptions() sets.String
+	processFoundState(foundStatusInSchema bool, crdItem apiextensionsv1.CustomResourceDefinition, versionIndex int) (bool, string)
+}
+
 var _ = g.Describe("[sig-arch][Early]", func() {
 
 	defer g.GinkgoRecover()
@@ -153,7 +257,9 @@ var _ = g.Describe("[sig-arch][Early]", func() {
 
 	g.Describe("CRDs for openshift.io", func() {
 		g.It("should have subresource.status", func() {
-			failures := checkSubresourceStatus(crdItemList)
+			subresourceStatusChecker := checkSubresourceStatusType{}
+			failures := checkFunc(subresourceStatusChecker, crdItemList)
+			//checkSubresourceStatus(crdItemList)
 			if len(failures) > 0 {
 				e2e.Fail(strings.Join(failures, "\n"))
 			}
@@ -180,7 +286,10 @@ var _ = g.Describe("[sig-arch][Early]", func() {
 
 	g.Describe("CRDs for openshift.io", func() {
 		g.It("should have a status in the CRD schema", func() {
-			failures := checkStatusInSchema(crdItemList)
+
+			statusInSchemaChecker := checkStatusInSchemaType{}
+			failures := checkFunc(statusInSchemaChecker, crdItemList)
+			//failures := checkStatusInSchema(crdItemList)
 			if len(failures) > 0 {
 				e2e.Fail(strings.Join(failures, "\n"))
 			}
